f2453786006e846c2a8b331d2d87b01c45c74384
DoG-peer <dogeometry50@gmail.com>
2017-03-15 23:00:33 +0900

    モデルを試行錯誤

--- main.py
+++ main.py
@@ -1,18 +1,136 @@
-image_dir = './images'
-out_image_dir = './out_images'
-out_model_dir = './out_models'
+import tensorflow as tf
+import numpy as np
+import os
+import sys
+import argparse
+from PIL import Image
+from model import DCGANInput, DCGANModel
+from input_data import MNISTTrainInput, CelebAInput
 
-nz = 100
-batch_size = 100
-n_epoch = 1e4
-n_train = 2e5
-image_save_interval = 5e4
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--task", default="run",
+                      choices=["train", "run", "run_continuous"],
+                      help="実行内容[train, run, run_continuous]")
+  parser.add_argument("--input", help="学習データの種類")
+  parser.add_argument("--img_dir", default="./images", help="学習画像ディレクトリ")
+  parser.add_argument("--out_img_dir", default="./out_images",
+                      help="画像出力ディレクトリ")
+  parser.add_argument("--model_dir", default="./save",
+                      help="モデル出力ディレクトリ")
+  parser.add_argument("--checkpoint", help="保存されたモデルのパス")
+  parser.add_argument("--nz", type=int, default=100, help="zの数")  # 現状意味なし
+  parser.add_argument("--batch_size", type=int, default=100, help="バッチサイズ")
+  parser.add_argument("--n_epoch", type=int, default=10000, help="エポック数")
+  parser.add_argument("--n_train", type=int, default=200000, help="学習回数")
+  parser.add_argument("--image_save_interval", type=int,
+                          default=50000, help="学習途中画像生成頻度。0以下のときは行わない")
+  return parser.parse_args()
 
-fs = os.listdir(image_dir)
 
-dataset = []
-for fn in fs:
-  with open('%s/%s' % (image_dir, fn), 'rb') as f:
-    img_bin = f.read()
-    dataset.append(img_bin)
+def save_images(imgs, img_name, latest_img_name):
+  n, h, w, _ = imgs.shape
+  sq = int(np.ceil(np.sqrt(n)))
+  img_base = Image.fromarray(np.zeros([sq * h, sq * w, 3], dtype=np.uint8))
+  for i, img in enumerate(imgs):
+    img_base.paste(Image.fromarray(img), [h * (i // sq), w * (i % sq)])
+  img_base.save(img_name)
+  img_base.save(latest_img_name)
 
+def train(args):
+  # 学習
+
+  # モデルを作成
+  model = DCGANModel(args.batch_size)
+
+  if args.input == "mnist":
+    dcgan_input = MNISTTrainInput(args.img_dir, args.batch_size, True)
+  elif args.input == "celeba":
+    dcgan_input = CelebAInput(args.img_dir, args.batch_size, True)
+  else:
+    dcgan_input = DCGANInput(args.img_dir, args.batch_size, True)
+  dcgan_input.connect(model)
+
+  # 学習の進行状況の確認
+  train_state = {"latest_epoch": 0}
+  logger = Logger(args.model_dir, args.checkpoint)
+  k = list(model._outputs.keys())[0]
+  # print(model._outputs)
+  l_g, l_d0, l_d1, l_reg = model._outputs[k][4:]
+  with tf.Session() as sess:
+    model.init(sess, logger)  # 途中データがあれば読み込み
+    # for v in tf.global_variables():
+    #   print(v.name, v.get_shape())
+    for epoch in range(train_state["latest_epoch"], args.n_epoch + 1):
+      # epochは1スタート
+      print("start epoch %d" % epoch)
+      for i in range(1, args.n_train + 1, args.batch_size):
+        sys.stdout.write("epoch %d, step %d-%d\r" % (epoch, i, i + args.batch_size - 1))
+        feed_dict = dcgan_input.feed_dict()
+        # sess.run([model.bn_updates_gen, model.train_gen, model.bn_updates, model.train_dis, model.train_reg], feed_dict)
+        # sess.run([model.bn_updates_gen, model.train_gen, model.bn_updates, model.train_dis], feed_dict)
+        # sess.run([model.bn_updates, model.train_gen, model.train_dis, model.train_reg], feed_dict)
+        sess.run([model.train_gen, model.train_dis, model.bn_updates, model.train_reg], feed_dict)
+        # sess.run([model.train_gen, model.bn_updates_gen], feed_dict)
+        # sess.run([model.train_dis, model.bn_updates_dis, model.train_reg], feed_dict)
+        # sess.run([model.train_dis, model.bn_updates, model.train_gen, model.train_reg], feed_dict)
+        # sess.run([model.train_gen, model.train_dis, model.train_reg, model.bn_updates, ], feed_dict)
+        # sess.run([model.train_gen, model.bn_updates_gen], feed_dict)
+        # sess.run([model.train_dis, model.bn_updates, model.train_reg], feed_dict)
+        # sess.run([model.train_gen, model.train_dis, model.bn_updates, model.bn_updates_gen], feed_dict)
+
+        if args.image_save_interval > 0 and (i + args.batch_size - 1) % args.image_save_interval == 0:
+          imgs = model.generate_image(sess, args.nz)
+          save_images(imgs,
+                      os.path.join(args.out_img_dir, "output_%06d_%06d.png" % (epoch, i + args.batch_size - 1)),
+                      os.path.join(args.out_img_dir, "_latest.png"))
+          print(sess.run([l_g, l_d0, l_d1, l_reg], dcgan_input.feed_dict()))
+
+      print("\nfinish epoch %d" % epoch)
+      logger.save_model(sess)
+      print("saved")
+
+class Logger:
+  def __init__(self, model_dir, checkpoint=None):
+    self.model_dir = model_dir
+    self.saver = tf.train.Saver()
+    self.checkpoint = checkpoint
+
+  def save_model(self, sess):
+    self.saver.save(sess, os.path.join(self.model_dir, "dcgan_model.ckpt"))
+
+  def restore(self, sess):
+    self.saver.restore(sess, self.checkpoint)
+
+def run(args):
+  assert args.checkpoint is not None
+  model = DCGANModel(args.batch_size)
+  dcgan_input = CelebAInput(args.img_dir, args.batch_size, False)
+  dcgan_input.connect(model)
+  logger = Logger(args.model_dir, args.checkpoint)
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    model.init(sess, logger)
+    imgs = model.generate_image(sess, args.nz)
+
+  model.generate_image()
+  print("未実装")
+
+
+def run_continuous(args):
+  # 連続的に画像生成
+  print("未実装")
+
+
+def main():
+  args = parse_args()
+  if args.task == "train":
+    train(args)
+  elif args.task == "run":
+    run(args)
+  elif args.task == "run_continuous":
+    run_continuous(args)
+
+
+if __name__ == '__main__':
+  main()
--- model.py
+++ model.py
@@ -1,179 +1,336 @@
 import pickle
 import os
 import math
+import re
 
 import numpy as np
 from PIL import Image
-import pylab
 
 import tensorflow as tf
 from tensorflow.contrib import layers
 
+def straight_log(x, k):
+  # kは最大の傾き
+  return tf.where(x < 1 / k, k * x - np.log(k) - 1, tf.log(x), "straight_log")
+
+def non_color_loss(x):
+  return tf.reduce_mean(tf.maximum(x * x - 1, 0))
+
 class Generator:
   def __init__(self):
-    pass
+    self.output = None
 
-  def __call__(self, zs, is_training=False):
+  def __call__(self, zs, is_training=False, reuse=False):
     # zs.shape: [*, nz]
-    bn_params = {'decay': 0.9, 'scale': True, 'is_training': is_training}
-    h = layers.fully_connected(zs, 6 * 6 * 512,
+    nz = zs.get_shape().as_list()[-1]
+    s = 6
+    # s = 2
+    h = layers.fully_connected(zs, s * s * 512,
         normalizer_fn=layers.batch_norm,
-        normalizer_params=bn_params,
-        weights_initializer=tf.random_normal_initializer(stddev=0.02 * np.sqrt(nz)))
-    h = tf.reshape(h, [-1, 6, 6, 512])
+        normalizer_params=_bn_params("bn_gen_fc", is_training, reuse),
+        weights_initializer=_initializer,
+        reuse=reuse,
+        scope="generator_fc")
+    h = tf.reshape(h, [-1, s, s, 512])
     input_dims = [512, 256, 128, 64]
     output_dims = [256, 128, 64, 3]
     for i in range(4):
-      stddev = 0.02 * np.sqrt(input_dims[i] * 4 * 4)
       h = layers.conv2d_transpose(
           h, output_dims[i], 4,
           stride=2, padding="SAME",
-          weights_initializer=tf.random_normal_initializer(stddev=stddev),
+          weights_initializer=_initializer_deconv,
+          weights_regularizer=layers.l2_regularizer(1e-5),
           normalizer_fn=layers.batch_norm if i != 3 else None,
-          normalizer_params=bn_params if i != 3 else None,
-          activation_fn=tf.nn.relu if i != 3 else tf.identity)
-    return h  # shape: [*, h, w, 3]
+          normalizer_params=_bn_params("bn_gen_%d" % i, is_training, reuse) if i != 3 else None,
+          activation_fn=tf.nn.relu if i != 3 else None,
+          reuse=reuse,
+          scope="generator_conv_%02d" % i)
+      # print(h)
+    self.output = h
+    # self.output = tf.tan(h)
+    # self.output = tf.tanh(h) * 2
+    return self.output  # shape: [*, h, w, 3]
+
+  def generate_image(self, sess, n):
+    z = tf.random_uniform([n, 100], -1, 1, dtype=tf.float32)
+    out = self(z, is_training=False, reuse=True)
+    return _to_imgs(sess.run(out))
+
+def _to_imgs(imgs):
+  return np.uint8(np.clip((imgs + 1) * 128, 0, 255))
+
+
+def _initializer(shape, dtype, partition_info):
+  # stddev = 0.02
+  f_in = np.prod(shape[:-1])
+  # stddev = np.sqrt(0.02 / np.sqrt(f_in))
+  stddev = np.sqrt(0.02 / np.sqrt(f_in))
+  # stddev = np.sqrt(0.02 / f_in)
+  return tf.random_normal_initializer(stddev=stddev)(shape, dtype=dtype, partition_info=partition_info)
+
+def _initializer_deconv(shape, dtype, partition_info):
+  # stddev = 0.02
+  f_in = np.prod(shape[:-2]) * shape[-1]
+  # stddev = np.sqrt(0.02 / np.sqrt(f_in))
+  stddev = np.sqrt(0.02 / np.sqrt(f_in))
+  # stddev = np.sqrt(0.02 / f_in)
+  return tf.random_normal_initializer(stddev=stddev)(shape, dtype=dtype, partition_info=partition_info)
+
+def _bn_params(name, is_training, reuse):
+  bn_params = {'decay': 0.9,
+               'is_training': is_training,
+               'fused': True,
+               'scope': name,
+               'reuse': reuse,
+               'epsilon': 2e-5}
+  """
+  bn_params = {'decay': 0.9,
+               'scale': True,
+               'is_training': is_training,
+               'fused': True,
+               'scope': name,
+               'reuse': reuse,
+               'epsilon': 2e-5}
+  """
+  return bn_params
+
 
 class Discriminator:
   def __init__(self):
     pass
 
-  def __call__(self, x, is_training=False):
+  def __call__(self, x, is_training=False, reuse=False):
     # x.shape: [batch_size, 96, 96, 3]
     h = x
-    bn_params = {'decay': 0.9, 'scale': True, 'is_training': is_training}
+
     input_dims = [3, 64, 128, 256]
     output_dims = [64, 128, 256, 512]
     for i in range(4):
-      stddev = 0.02 * np.sqrt(input_dims[i] * 4 * 4)
       h = layers.conv2d(
           h, output_dims[i], 4, stride=2, padding='SAME',
-          weights_initializer=tf.random_normal_initializer(stddev=stddev),
+          weights_initializer=_initializer,
+          weights_regularizer=layers.l2_regularizer(1e-5),
           activation_fn=tf.nn.elu,
           normalizer_fn=layers.batch_norm if i != 0 else None,
-          normalizer_params=bn_params if i != 0 else None)
+          normalizer_params=_bn_params("bn_dis_%d" % i, is_training, reuse) if i != 0 else None,
+          reuse=reuse,
+          scope="discriminator_conv_%02d" % i)
+
+    """
+    # h: [-1, 6, 6, 512]
+    z = layers.conv2d(h, 1, 4, stride=1, padding='VALID',
+        weights_initializer=tf.random_normal_initializer(stddev=0.02),
+        weights_regularizer=layers.l2_regularizer(1e-5),
+        activation_fn=tf.sigmoid,
+        reuse=reuse,
+        scope="discriminator_prob")
+    """
+
+    #"""
     h = layers.flatten(h)
 
-    stddev = 0.02 * np.sqrt(h.get_shape().as_list()[-1])
     z = layers.fully_connected(
         h, 2,
-        weights_initializer=tf.random_normal_initializer(stddev=stddev))
-    return z  # shape: [batch_size, 2]
+        weights_initializer=_initializer,
+        reuse=reuse,
+        activation_fn=None,
+        scope="discriminator_fc")
+    #"""
+
+    """
+    h = layers.flatten(h)
+
+    stddev = np.sqrt(0.02 / np.sqrt(h.get_shape().as_list()[-1]))
+    # stddev = np.sqrt(0.02 * np.sqrt(h.get_shape().as_list()[-1]))
+    # stddev = 0.02
+    z = layers.fully_connected(
+        h, 1,
+        weights_initializer=tf.random_normal_initializer(stddev=stddev),
+        reuse=reuse,
+        activation_fn=None,  # tf.sigmoid,
+        scope="discriminator_fc")
+    """
+    return z  # shape: [batch_size, 2] / [batch_size, 6, 6, 2]
 
 
-def train_dcgan_labeled(gen, dis, epoch0=0):
-  opt_gen = tf.train.AdamOptimizer() # alpha=0.0002, beta1=0.5, decay=0.00001
-  opt_dis = tf.train.AdamOptimizer() # alpha=0.0002, beta1=0.5, decay=0.00001
-  h = w = 96
-  z = tf.random_uniform([batch_size, nz], -1, 1, dtype=tf.float32)
-  x_ = tf.placeholder(tf.float32, [batch_size, h, w, 3])
+class DCGANModel:
+  def __init__(self, z_batch_size=100, nz=100, *params):
+    self._used = False
+    self._inputs = []
+    self._outputs = {}  # keyは_inputsのオブジェクト、valueは??
+    self._gen = Generator()
+    self._dis = Discriminator()
+    self._z_batch_size = z_batch_size
+    self._nz = nz
 
-  x = gen(z)
-  yl_0 = dis(x)  # shape: [batch_size, 2]
-  l_gen = tf.reduce_mean(-tf.nn.log_softmax(yl_0)[:, 0])
-  l_dis_0 = tf.reduce_mean(-tf.nn.log_softmax(yl_0)[:, 1])
+  def init(self, sess, logger):
+    if logger.checkpoint is not None:
+      logger.restore(sess)
+    else:
+      sess.run(tf.global_variables_initializer())
 
-  yl_1 = dis(x_)
-  l_dis_1 = tf.reduce_mean(-tf.nn.log_softmax(yl_1)[:, 0])
-  l_dis = l_dis_0 + l_dis_1
+  def assgin_input(self, input_layer, is_training=True):
+    reuse = self._used
+    self._inputs.append(input_layer)
+    with tf.name_scope("dcgan"):
+      if is_training:
+        z = tf.random_uniform([self._z_batch_size, self._nz], -1, 1, dtype=tf.float32)
+        x = self._gen(z, is_training=is_training, reuse=reuse)
 
-  train_gen = tf.train.AdamOptimizer()  # TODO: (alpha=0.0002, beta1=0.5)WeightDecay(0.00001))
-  train_dis = tf.train.AdamOptimizer()
+        """
+        yl_0 = self._dis(x, is_training=is_training, reuse=reuse)
+        yl_1 = self._dis(input_layer, is_training=is_training, reuse=True)
+        """
 
-  update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-  bn_updates = tf.group(*update_ops)
-  # TODO: GeneratorとDiscriminatorそれぞれで別れて更新できるようにしたい
-  with tf.Session() as sess:
-    for epoch in range(epoch0, n_epoch):
-      for i in range(0, n_train, batch_size):
-        sess.run(train_gen)
-        sess.run(train_dis, {x_: get_batch(batch_size)})
-        sess.run(bn_updates)
-nz = 100
-batch_size = 32
-train_dcgan_labeled(Generator(), Discriminator())
-"""
-  o_gen = optimizers.Adam(alpha=0.0002, beta1=0.5)
-  o_dis = optimizers.Adam(alpha=0.0002, beta1=0.5)
-  o_gen.setup(gen) ??
-  o_dis.setup(dis) ??
-  o_gen.add_hook(chainer.optimizer.WeightDecay(0.00001)) ??
-  o_dis.add_hook(chainer.optimizer.WeightDecay(0.00001)) ??
+        """
+        yl_0_g = self._dis(x, is_training=False, reuse=reuse)
+        yl_0_d = self._dis(x, is_training=is_training, reuse=True)
+        yl_1 = self._dis(input_layer, is_training=is_training, reuse=True)
+        """
 
-  for epoch in xrange(epoch0,n_epoch):
-    perm = np.random.permutation(n_train)
-    sum_l_dis = np.float32(0)
-    sum_l_gen = np.float32(0)
+        # """
+        join = tf.concat([x, input_layer], 0)
+        yl = self._dis(join, is_training=is_training, reuse=reuse)
+        yl_0 = yl[:self._z_batch_size]
+        yl_1 = yl[self._z_batch_size:]
+        # """
 
-    for i in xrange(0, n_train, batchsize):
-      # discriminator
-      # 0: from dataset
-      # 1: from noise
+        """
+        l_gen = tf.reduce_mean(-tf.nn.log_softmax(yl_0_g)[:, 0])
+        l_dis_0 = tf.reduce_mean(-tf.nn.log_softmax(yl_0_d)[:, 1])
+        l_dis_1 = tf.reduce_mean(-tf.nn.log_softmax(yl_1)[:, 0])
+        """
 
-      #print "load image start ", i
-      x2 = np.zeros((batchsize, 3, 96, 96), dtype=np.float32)
-      for j in range(batchsize):
-        try:
-          rnd = np.random.randint(len(dataset))
-          rnd2 = np.random.randint(2)
+        """
+        l_gen = - tf.reduce_mean(tf.nn.log_softmax(yl_0)[:, 0])
+        l_dis_0 = - tf.reduce_mean(tf.nn.log_softmax(yl_0)[:, 1])
+        l_dis_1 = - tf.reduce_mean(tf.nn.log_softmax(yl_1)[:, 0])
+        """
 
-          img = np.asarray(Image.open(StringIO(dataset[rnd])).convert('RGB')).astype(np.float32).transpose(2, 0, 1)
-          if rnd2==0:
-            x2[j,:,:,:] = (img[:,:,::-1]-128.0)/128.0
-          else:
-            x2[j,:,:,:] = (img[:,:,:]-128.0)/128.0
-        except:
-          print 'read image error occured', fs[rnd]
-      #print "load image done"
+        # """
+        epsi = 1e-10
+        # l_gen = - tf.reduce_mean(straight_log(tf.nn.softmax(yl_0), 100)[:, 0])
+        # l_gen = - tf.reduce_mean(tf.log(tf.nn.softmax(yl_0) + epsi)[:, 0])
+        l_gen = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=yl_0, labels=tf.zeros([self._z_batch_size], dtype=tf.int32)))
+        # l_gen = tf.reduce_mean(tf.nn.softmax(yl_0)[:, 1])
+        # l_gen = tf.reduce_mean(tf.tan(tf.nn.softmax(yl_0) * np.pi / 2)[:, 1])
 
-      # train generator
-      z = Variable(xp.random.uniform(-1, 1, (batchsize, nz), dtype=np.float32))
-      x = gen(z)
-      yl = dis(x)
-      L_gen = F.softmax_cross_entropy(yl, Variable(xp.zeros(batchsize, dtype=np.int32)))
-      L_dis = F.softmax_cross_entropy(yl, Variable(xp.ones(batchsize, dtype=np.int32)))
+        l_dis_0 = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=yl_0, labels=tf.ones_like(yl_0[:, 0], dtype=tf.int32)))
+        # l_dis_0 = tf.reduce_mean(tf.nn.softmax(yl_0)[:, 0])
+        # l_dis_0 = tf.reduce_mean(tf.nn.sigmoid(yl_0)[:, 0])
+        # l_dis_0 = - tf.reduce_mean(tf.log(tf.maximum(tf.nn.softmax(yl_0), epsi))[:, 1])
 
-      # train discriminator
+        l_dis_1 = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=yl_1, labels=tf.zeros_like(yl_0[:, 0], dtype=tf.int32)))
+        # l_dis_1 = tf.reduce_mean(tf.nn.softmax(yl_1)[:, 1])
+        # l_dis_1 = tf.reduce_mean(tf.nn.sigmoid(yl_1)[:, 1])
+        # l_dis_1 = - tf.reduce_mean(tf.log(tf.maximum(tf.nn.softmax(yl_1), epsi))[:, 0])
+        # """
 
-      x2 = Variable(cuda.to_gpu(x2))
-      yl2 = dis(x2)
-      L_dis += F.softmax_cross_entropy(yl2, Variable(xp.zeros(batchsize, dtype=np.int32)))
+        """
+        epsi = 1e-10
+        l_gen = tf.reduce_mean(-tf.log(yl_0 + epsi))
+        l_dis_0 = tf.reduce_mean(-tf.log(1 - yl_0 + epsi))
+        # l_dis_0 = tf.reduce_mean(tf.log(yl_0 + epsi))
+        l_dis_1 = tf.reduce_mean(-tf.log(yl_1 + epsi))
+        """
 
-      #print "forward done"
+        """
+        l_gen = tf.reduce_mean(- yl_0 + tf.log(tf.exp(yl_0) + tf.exp(- yl_0)))
+        l_dis_0 = tf.reduce_mean(yl_0 + tf.log(tf.exp(yl_0) + tf.exp(- yl_0)))
+        l_dis_1 = tf.reduce_mean(- yl_1 + tf.log(tf.exp(yl_1) + tf.exp(- yl_1)))
+        """
+        l_gen = l_gen + non_color_loss(x)
+        l_dis = l_dis_0 + l_dis_1
+        # TODO: 暫定的な実装
+        vars_gen = []
+        vars_dis = []
+        for v in tf.global_variables():
+          # print(v.name, v.get_shape())
+          """
+          if re.match("bn_", v.name):
+            vars_gen.append(v)
+            vars_dis.append(v)
+          elif re.match("generator", v.name):
+          """
+          if re.match("generator", v.name):
+            # print("\tgen")
+            if v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
+              # print("\ttrain")
+              vars_gen.append(v)
+          elif re.match("discriminator", v.name):
+            # print("\tdis")
+            if v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
+              # print("\ttrain")
+              vars_dis.append(v)
+        # exit()
+        # train_gen = tf.train.AdamOptimizer(1e-4, beta1=0.5).minimize(l_gen, var_list=vars_gen)
+        # train_dis = tf.train.AdamOptimizer(1e-4, beta1=0.5).minimize(l_dis, var_list=vars_dis)
+        # train_gen = tf.train.AdamOptimizer(2e-4, beta1=0.5).minimize(l_gen, var_list=vars_gen)
+        train_gen = tf.train.AdamOptimizer(2e-4, beta1=0.5).minimize(l_gen, var_list=vars_gen)
+        train_dis = tf.train.AdamOptimizer(2e-4, beta1=0.5).minimize(l_dis, var_list=vars_dis)
 
-      o_gen.zero_grads()
-      L_gen.backward()
-      o_gen.update()
+        # train_gen = tf.train.GradientDescentOptimizer(0.0001).minimize(l_gen, var_list=vars_gen)
+        # train_dis = tf.train.GradientDescentOptimizer(0.0001).minimize(l_dis, var_list=vars_dis)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        bn_updates = tf.group(*update_ops)
+        bn_updates_gen = tf.group(*[v for v in update_ops if v.name.find("/bn_gen_") >= 0])
+        bn_updates_dis = tf.group(*[v for v in update_ops if v.name.find("/bn_dis_") >= 0])
 
-      o_dis.zero_grads()
-      L_dis.backward()
-      o_dis.update()
+        l_reg = sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+        train_reg = tf.train.GradientDescentOptimizer(1.).minimize(l_reg)
+        outputs = [train_gen, train_dis, bn_updates, train_reg, l_gen, l_dis_0, l_dis_1, l_reg]
+        self._outputs[input_layer] = outputs  # 学習時と実行時は異なるinput_layerを使う
 
-      sum_l_gen += L_gen.data.get()
-      sum_l_dis += L_dis.data.get()
+        # デフォルトのopを更新
+        self.train_gen = train_gen
+        self.train_dis = train_dis
+        self.train_reg = train_reg
+        self.bn_updates = bn_updates
+        self.bn_updates_dis = bn_updates_dis
+        self.bn_updates_gen = bn_updates_gen
 
-      #print "backward done"
+      else:
+        z = tf.random_uniform([self._z_batch_size, self._nz], -1, 1, dtype=tf.float32)
+        x = self._gen(z, is_training=is_training, reuse=reuse)
+        yl_0 = self._dis(x, is_training=is_training, reuse=reuse)
 
-      if i%image_save_interval==0:
-          pylab.rcParams['figure.figsize'] = (16.0,16.0)
-          pylab.clf()
-          vissize = 100
-          z = zvis
-          z[50:,:] = (xp.random.uniform(-1, 1, (50, nz), dtype=np.float32))
-          z = Variable(z)
-          x = gen(z, test=True)
-          x = x.data.get()
-          for i_ in range(100):
-              tmp = ((np.vectorize(clip_img)(x[i_,:,:,:])+1)/2).transpose(1,2,0)
-              pylab.subplot(10,10,i_+1)
-              pylab.imshow(tmp)
-              pylab.axis('off')
-          pylab.savefig('%s/vis_%d_%d.png'%(out_image_dir, epoch,i))
+    self._used = True
+    return self._outputs[input_layer] if is_training else None
 
-    serializers.save_hdf5("%s/dcgan_model_dis_%d.h5"%(out_model_dir, epoch),dis)
-    serializers.save_hdf5("%s/dcgan_model_gen_%d.h5"%(out_model_dir, epoch),gen)
-    serializers.save_hdf5("%s/dcgan_state_dis_%d.h5"%(out_model_dir, epoch),o_dis)
-    serializers.save_hdf5("%s/dcgan_state_gen_%d.h5"%(out_model_dir, epoch),o_gen)
-    print 'epoch end', epoch, sum_l_gen/n_train, sum_l_dis/n_train
-"""
+  def generate_image(self, sess, n):
+    # n枚の画像を生成する(nの指定は現状放棄する)
+    return self._gen.generate_image(sess, n)
 
+  def restore(self):
+    pass
+
+# TrainInputとRunInputに分ける
+class DCGANInput:
+  def __init__(self, batch_size, is_training, h=96, w=96):
+    self.batch_size = batch_size
+    self.is_training = is_training
+    self.input_layer = tf.placeholder(tf.float32, [batch_size, h, w, 3]) # or tf.int8
+
+  def connect(self, model):
+    model.assgin_input(self._imgs_to_train_input(self.input_layer), is_training=self.is_training)
+  def load_images(self, n):
+    return np.zeros([n, 96, 96, 3], dtype=np.float32)
+    # return np.zeros([n, 28, 28, 3], dtype=np.float32)
+
+  def _imgs_to_train_input(self, imgs_rgb):
+    # 学習時に使うデータのおよそ半分をbgrで行う
+    # 入力のスケールは値が-1から1に入るようにする
+    imgs_rgb = tf.cast(imgs_rgb, tf.float32)
+    imgs_bgr = imgs_rgb[..., ::-1]
+    rnd = tf.random_uniform(minval=0, maxval=2, dtype=tf.int32, shape=[self.batch_size])
+    rnd = tf.cast(rnd, tf.float32)
+    rnd = tf.reshape(rnd, [-1, 1, 1, 1])
+    imgs = imgs_rgb * rnd + imgs_bgr * (1 - rnd)
+    # imgs = imgs_rgb  # * rnd + imgs_bgr * (1 - rnd)
+    imgs = (imgs - 128) / 128
+
+    return imgs
+
+  def feed_dict(self):
+    imgs_rgb = self.load_images(self.batch_size)
+    return {self.imgs_rgb: imgs_rgb}
